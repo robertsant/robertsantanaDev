507605bf42d846a6dfadb2fccbde9e955ff5b61c
robertsant <robertsantana0412@gmail.com>
2023-05-18 11:41:29 -0300

    1 teste de page

---
+++ script.js
@@ -0,0 +1,123 @@
+// let idade = 20;
+// console.log(idade)
+// let altura = 190
+// console.log(altura)
+
+
+// let nomeCompleto = 'Robert da cruz santana'
+// console.log(nomeCompleto)
+// let curso = 'tecnologia'
+// console.log(curso)
+// let clima = 'frio' 
+// console.log(clima)
+
+
+//const valorIngressoAdulto = 20;
+
+//console.log(valorIngressoAdulto);
+
+
+// let familia = [12,25,24,47];
+
+
+// console.log(familia[1 ]); 
+
+
+
+// let layoutsite = "loginesenha"
+
+
+
+// let corParede = "blue";
+// function resetaCor(cor,tonalidade){
+//     corParede = cor + " " + tonalidade;
+
+// };
+
+// console.log(corParede);
+
+// resetaCor("azul","escuro")
+// console.log(corParede)
+
+
+
+// let rua = "augostinho";
+// console.log(rua);
+
+
+// function dizerNome(){
+//     console.log('Robert');
+// }
+// dizerNome();
+
+
+
+// function MultiplicarPorDois(valor){
+//     return valor * 2;
+
+// }
+
+// //console.log(MultiplicarPorDois(5));
+
+// let resultado = MultiplicarPorDois(5)
+// console.log (resultado); 
+
+// let salario = 100
+
+// //+,*,-,/,**
+
+// console.log(salario - salario);
+
+
+// let idade = 18
+// console.log(--idade);
+
+
+// console.log (2===2)
+
+// console.log(3===3)
+
+// let pontos = 99;
+// let tipo = pontos > 100 ? 'premium' : 'comum';
+// console.log(tipo);
+
+
+//operadores logico e (&&)
+
+
+//console.log(true && true);
+
+
+// let MaiorDeIdade = false;
+// let PossuiCarteiraDeTrabalho = false;
+// let podeAplicar = MaiorDeIdade || PossuiCarteiraDeTrabalho;
+// console.log (podeAplicar)
+
+
+// let candidatoRecusado = !podeAplicar;
+// console.log(candidatoRecusado);         
+
+
+ 
+// let corPersonalizada = 'yellow';
+// let corPadrao = 'black';
+// let corPerfil = corPersonalizada || corPadrao;
+// console.log(corPerfil);
+
+
+
+let a = 'vermelho';
+let b = 'azul';
+let c = a;
+a = b;
+b = c;
+console.log (a);
+console.log (b);
+
+
+
+
+
+
+
+
